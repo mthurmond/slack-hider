4d899ebcb96992bd2996bb7f3aed4c86f25c7125
Matt Thurmond <matt.thurmond@gmail.com>
2021-01-15 13:43:07 -0500

    Refactor injected badge styles

--- hider/hider.js
+++ hider/hider.js
@@ -1,5 +1,5 @@
 //store slack's "no new messages" favicon by loading the image from the .crx file using the chrome extension API's ".getURL" method. 
-const noMessageFavicon = chrome.extension.getURL("/hider/favicon-no-messages.png");
+const noMessageFavicon = chrome.extension.getURL('/hider/favicon-no-messages.png');
 
 //create flag to control whether messages sidebar should be hidden. set value to true and remove initial toggleMessages function call to show messages by default. set to false and include an initial toggleMessages call to hide messages by default.
 let showMessages = false;
@@ -55,7 +55,7 @@ function swapFavicon(showFavicon) {
         chrome.storage.sync.set({ 'faviconValue': lastFavicon }, function () { });
         document.querySelector('link[rel*="icon"]').href = noMessageFavicon;
 
-        //set mutation observer that swaps the "no message" favicon back in if it's ever changed while messages are hidden. have to observe the "head" element because slack swaps out the  full favicon element whenever the favicon needs to be changed. 
+        //set mutation observer that swaps the "no message" favicon back in if it's ever changed while messages are hidden. have to observe the "head" element because slack swaps out the full favicon element whenever the favicon needs to be changed. 
         faviconObserver = new MutationObserver(function(mutations) {
             if (!showMessages && document.querySelector('link[rel*="icon"]').href != noMessageFavicon) {
                 document.querySelector('link[rel*="icon"]').href = noMessageFavicon;
@@ -63,7 +63,7 @@ function swapFavicon(showFavicon) {
         });
 
         faviconObserver.observe(
-            document.querySelector("head"),
+            document.querySelector('head'),
             {subtree: true, characterData: true, childList: true, attributes: true}
         );
 
@@ -85,8 +85,8 @@ function swapTitle(showDefaultTitle) {
 
         //activate the mutation observer
         titleObserver = new MutationObserver(function(mutations) {
-            if (!showMessages && document.title != "Messages hidden") {
-                document.title = "Messages hidden";
+            if (!showMessages && document.title != 'Messages hidden') {
+                document.title = 'Messages hidden';
             } 
         });
 
@@ -98,62 +98,45 @@ function swapTitle(showDefaultTitle) {
     }
 }
 
-//remove all injected css rules
-const clearInjectedCSS = () => {
-    const injectedNode = document.getElementById('slack-hider-injected');
-
-    if (injectedNode) {
-        injectedNode.parentNode.removeChild(injectedNode);
-    }
-}
-
-//pass a css ruleset, and this appends it to the document body
-function injectCSS(CSSRule) {
-    let nodeToInject = document.createElement('style');
-    nodeToInject.setAttribute('id', 'slack-hider-injected');
-    nodeToInject.innerHTML = CSSRule;
-    document.body.appendChild(nodeToInject);
-}
-
-//object to store css rulesets that need to be added/removed based on message visibility
-selectors = {
-    //create rulsets to hide the unread message badges that appear in slack search results
-    'Slack search unread badges - people': function (value) { return `.c-member__unread_count { display: ${value}; }` },
-    'Slack search unread badges - channels': function (value) { return `.c-channel_entity__mentions { display: ${value}; }` },
-}
-
 //called when show/hide button clicked, with current "showMessages" boolean value. clicking the button adjusts the sidebar visibility and button text.  
 function toggleMessages(areMessagesVisible) {
-    const slackChannelSidebar = getSidebar();
     
-    //store the appropriate visibility and display css values
-    const elementVisibility = areMessagesVisible ? 'visible' : 'hidden';
-    const elementDisplay = areMessagesVisible ? 'flex' : 'none';
-
-    slackChannelSidebar.style.visibility = elementVisibility;
+    //set sidebar visibility
+    const slackChannelSidebar = getSidebar();
+    const sidebarVisibility = areMessagesVisible ? 'visible' : 'hidden';
+    slackChannelSidebar.style.visibility = sidebarVisibility;
 
     messageToggleButton.innerHTML = areMessagesVisible ? 'Hide messages' : 'Show messages';
+
+    //set badge element display so mention badges only appear in search results while messages are visible
+    const badgeDisplay = areMessagesVisible ? 'inline-block' : 'none';
+    const badgeStyleElement = document.getElementById('slack-hider-injected');
+    badgeStyleElement.innerHTML = `.c-mention_badge { display: ${badgeDisplay}; }`
 
     //swap favicon each time button pressed
     swapFavicon(areMessagesVisible);
 
-    // clear any css injected previously
-    clearInjectedCSS();
-
-    //inject css rulsets to show/hide unread message badges in the slack search results. add as separate css styles because the elements don't exist on the page until the user begins a search, and, even after slack's default settings are applied, these separate styles will over-ride them.
-    injectCSS(selectors['Slack search unread badges - people'](elementDisplay));
-    injectCSS(selectors['Slack search unread badges - channels'](elementDisplay));
-
-    //each time button pressed, swap title
+    //swap title each time button pressed
     swapTitle(areMessagesVisible)
 
     //set showMessages equal to its new, opposite value since areMessagesVisible was set to "!showMessages" in the click event handler. the new value must be stored in this global variable so it persists in the browser's memory, gets attached to the 'window' object, and has the correct updated value next time the button is clicked.
     showMessages = areMessagesVisible;
 }
 
+// adds styles to the page that will be adjusted based on whether messages are hidden
+function addStyles() {
+    //create style for mention badges
+    const badgeStyleElement = document.createElement('style');
+    badgeStyleElement.id = 'slack-hider-injected';
+    badgeStyleElement.innerHTML = '.c-mention_badge { display: inline-block; }';
+    document.body.appendChild(badgeStyleElement);
+}
+
 //once the required elements exist, this function initiates the slack hider
 function initiateSlackHider() {
     addToggleButton();
+    addStyles();
+    
     //call this to hide messages by default when slack is first loaded
     toggleMessages(showMessages);
 }
