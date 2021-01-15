72c2a2da1a7f75ad1c7fcc34d4ab72a8ac2c2c60
Matt Thurmond <matt.thurmond@gmail.com>
2021-01-15 13:50:09 -0500

    Cleanup code

--- popup/popup.js
+++ popup/popup.js
@@ -5,36 +5,36 @@ const slackHiderURL = `${githubURL}/mthurmond/slack-hider`;
 const feedbackURL = `${githubURL}/mthurmond/slack-hider/issues`;
 
 const benURL = `${githubURL}/tavva`;
-const quietSlackURL = `${benURL}/quiet-slack`;
+const quietSlackURL = `${githubURL}/tavva/quiet-slack`;
 
 const manageExtensionsURL = 'chrome://extensions/';
 const extensionsDetailsURL = `${manageExtensionsURL}?id=ghbbhaemcgjamohgkapgkdinhgdcpgim`;
 
 //open the appropriate new tab when the user clicks each link
-document.querySelector(".feedbackLink").addEventListener("click", function() {
+document.querySelector('.feedbackLink').addEventListener('click', function() {
     chrome.tabs.create({url: feedbackURL})
 });
 
-document.querySelector(".slackHiderLink").addEventListener("click", function() {
+document.querySelector('.slackHiderLink').addEventListener('click', function() {
     chrome.tabs.create({url: slackHiderURL})
 });
 
-document.querySelector(".extensionsDetailsLink").addEventListener("click", function() {
+document.querySelector('.extensionsDetailsLink').addEventListener('click', function() {
     chrome.tabs.create({url: extensionsDetailsURL})
 });
 
-document.querySelector(".manageExtensionsLink").addEventListener("click", function() {
+document.querySelector('.manageExtensionsLink').addEventListener('click', function() {
     chrome.tabs.create({url: manageExtensionsURL})
 });
 
-document.querySelector(".benLink").addEventListener("click", function() {
+document.querySelector('.benLink').addEventListener('click', function() {
     chrome.tabs.create({url: benURL})
 });
 
-document.querySelector(".mattLink").addEventListener("click", function() {
+document.querySelector('.mattLink').addEventListener('click', function() {
     chrome.tabs.create({url: mattURL})
 });
 
-document.querySelector(".quietSlackLink").addEventListener("click", function() {
+document.querySelector('.quietSlackLink').addEventListener('click', function() {
     chrome.tabs.create({url: quietSlackURL})
 });
